82ed879c8187bb160a47b0a38b2fa7139f9f0cf1
H3M3L <lllooolllzzz555@gmail.com>
2022-07-04 14:44:16 +0600

    laurel_sprout: nuke some packages while building

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,12 +4,42 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt Camera2 CarrierSetup
-LOCAL_OVERRIDES_PACKAGES += MyVerizonServices Music
-LOCAL_OVERRIDES_PACKAGES += OBDM_Permissions OPScreenRecord RecorderPrebuilt
-LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt SprintDM SprintHM
-LOCAL_OVERRIDES_PACKAGES += VzwOmaTrigger YouTube YouTubeMusicPrebuilt
+LOCAL_OVERRIDES_PACKAGES := \
+        arcore \
+        AmbientSensePrebuilt \
+        AppDirectedSMSService \
+        ConnMO \
+        Drive \
+        DCMO \
+        DevicePolicyPrebuilt \
+        DMService \
+        GCS \
+        GoogleCamera \
+        MaestroPrebuilt \
+        Maps \
+        MicropaperPrebuilt \
+        MyVerizonServices \
+        OBDM_Permissions \
+        obdm_stub \
+        PixelWallpapers2020 \
+        PixelWallpapers2021 \
+        RecorderPrebuilt \
+        SCONE \
+        SafetyHubPrebuilt \
+        ScribePrebuilt \
+        Showcase \
+        SprintDM \
+        SprintHM \
+        Snap \
+        SoundAmplifierPrebuilt \
+        Tycho \
+        TurboPrebuilt \
+        talkback \
+        USCCDM \
+        VZWAPNLib \
+        VzwOmaTrigger \
+        
 LOCAL_UNINSTALLABLE_MODULE := true
-LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_CERTIFICATE := Platform
 LOCAL_SRC_FILES := /dev/null
 include $(BUILD_PREBUILT)
