a49a8cdc96cd5223ad807da38c8816c530a1f678
H3M3L <lllooolllzzz555@gmail.com>
2022-08-24 00:21:49 +0600

    laurel_sprout: Update vendorsetup for elixir

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,4 @@
+# clone
+git clone --depth=1 https://github.com/ProjectElixir-Devices/android-kernel-xiaomi-laurel_sprout kernel/xiaomi/laurel_sprout
+git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_xiaomi_laurel_sprout vendor/xiaomi/laurel_sprout
 export SELINUX_IGNORE_NEVERALLOWS=true
